2171c1aa36c4b99a43bb66dff001f3fe4305bb8d
Cường Lê <cuonglh2807@gmail.com>
2016-08-30 23:47:17 +0700

    Cường Lê - Done feature "Tập Đọc"

--- app/src/main/java/com/khtn/tapdoclophai/TapdocDetailActivity.java
+++ app/src/main/java/com/khtn/tapdoclophai/TapdocDetailActivity.java
@@ -1,23 +1,31 @@
 package com.khtn.tapdoclophai;
 
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.khtn.tapdoclophai.pojo.BaiDoc;
 import com.khtn.tapdoclophai.pojo.BaiHoc;
+import com.khtn.tapdoclophai.utility.AudioPlayer;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class TapdocDetailActivity extends AppCompatActivity {
 
     TextView txtNoiDungBaiDoc;
     ImageView imgDetailBaiDoc;
+    ImageButton btnimgPlaySound;
+    private AssetFileDescriptor afd;
+    private AudioPlayer audioPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,21 +34,52 @@ public class TapdocDetailActivity extends AppCompatActivity {
 
         txtNoiDungBaiDoc = (TextView) findViewById(R.id.txtNoiDungBaiDoc);
         imgDetailBaiDoc = (ImageView) findViewById(R.id.imgbaidocDetails);
+        btnimgPlaySound = (ImageButton) findViewById(R.id.imgPlaySound);
+
+        try {
 
         Intent i = getIntent();
         Bundle bundle = i.getBundleExtra("BaiDocBundle");
+            final BaiDoc baiDoc = (BaiDoc) bundle.getSerializable("BaiDocDetails");
+            try {
+                afd = getAssets().openFd("audio/"+baiDoc.getAudio());
+                audioPlayer = new AudioPlayer(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+                btnimgPlaySound.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(audioPlayer.isPlaying()) {
+                            audioPlayer.restart();
+                        }
+                        audioPlayer.play();
+                    }
+                });
+            } catch (IOException e) {
+                Log.d("error sound : ",e.toString());
+            }
+            txtNoiDungBaiDoc.setText(baiDoc.getNoiDung());
 
-        BaiDoc baiDoc = (BaiDoc) bundle.getSerializable("BaiDocDetails");
-
-        txtNoiDungBaiDoc.setText(baiDoc.getNoiDung());
-        try{
-            InputStream inputStream = getAssets().open("img/"+baiDoc.getImgMinhHoa());
-            Drawable drawable = Drawable.createFromStream(inputStream,null);
-            imgDetailBaiDoc.setImageDrawable(drawable);
+            if (!baiDoc.getImgMinhHoa().isEmpty()) {
+                try {
+                    InputStream inputStream = getAssets().open("img/" + baiDoc.getImgMinhHoa());
+                    Drawable drawable = Drawable.createFromStream(inputStream, null);
+                    imgDetailBaiDoc.setImageDrawable(drawable);
+                } catch (Exception ex) {
+                    Log.d("Error loading img : ", ex.toString());
+                }
+            }
         }
         catch (Exception ex)
         {
-            Log.d("Error loading img : ",ex.toString());
+            txtNoiDungBaiDoc.setText("Không tìm thấy nội dung");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(audioPlayer!=null)
+        {
+            audioPlayer.release();
         }
     }
 }
