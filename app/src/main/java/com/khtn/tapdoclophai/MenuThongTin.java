8fb47c214d0bc1f04ad788a3ff69556a068a3338
1542107 <1542107@student.hcmus.edu.vn>
2016-08-31 00:54:30 +0700

    Minh Tuan - Update UI ThongTinActivity

--- app/src/main/java/com/khtn/tapdoclophai/MenuThongTin.java
+++ app/src/main/java/com/khtn/tapdoclophai/MenuThongTin.java
@@ -3,13 +3,22 @@ package com.khtn.tapdoclophai;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MenuThongTin extends AppCompatActivity {
 
+    TextView txtThongTin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu_thong_tin);
+
+        txtThongTin = (TextView) findViewById(R.id.txtThongTin);
+
+        txtThongTin.setText("Noi dung gioi thieu nhom/ gioi thieu ung dung ma ong nghi~ ra :)) thi dien vao day");
+        // Phan ong lam la dien doan text nay thoi ak :)
+        // lam xong thi commmit len repo cua ong , roi pull request cho tui.
+
     }
 }
